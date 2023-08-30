b863d7c96c1a2ab0b692c43538b95d9c3841e636
Haggai Khafizov <hafyok5777@gmail.com>
2023-08-30 18:35:27 +0300

    Add stock timer image and fix MainActivity to TimerActivity.kt

--- app/src/main/java/com/example/timerrest/TimerActivity.kt
+++ app/src/main/java/com/example/timerrest/TimerActivity.kt
@@ -6,41 +6,45 @@ import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.NumberPicker
+import android.widget.Toast
 import com.example.timerrest.databinding.ActivityMainBinding
+import java.lang.String.format
 
-class MainActivity : AppCompatActivity() {
+class TimerActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private var timer: CountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         val numberPicker: NumberPicker  = binding.numberPicker
         numberPicker.minValue = 0
-        numberPicker.maxValue = 9
-        numberPicker.wrapSelectorWheel = false
+        numberPicker.maxValue = (180 - 15) / 5 // Количество возможных значений
+
+        numberPicker.displayedValues = (15..180 step 5).map { it.toString() }.toTypedArray()
+        numberPicker.wrapSelectorWheel = true
 
         numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
-            binding.textView.text = "Выбранное значение: $newVal"
+            binding.textView.text = "Выбранное значение: ${numberPicker.displayedValues[newVal]}"
+            binding.timerMinutes.text = numberPicker.displayedValues[newVal] + ":00"
         }
 
         setContentView(binding.root)
-        binding.apply {
-            bStart.setOnClickListener {
-                startCountDownTimer(10000)
-            }
+        binding.bStart.setOnClickListener {
+            val selectedMinutes = numberPicker.displayedValues[numberPicker.value].toLong() // Выбранное количество минут
+            startCountDownTimer(selectedMinutes * 60 * 1000)
         }
+
     }
 
     private fun startCountDownTimer(timerMillis: Long){
         timer?.cancel()
-        timer = object : CountDownTimer(timerMillis, 1000){
+        timer = object : CountDownTimer(timerMillis, 1){
             override fun onTick(timeM: Long) {
-                binding.timerMinutes.text = (timeM / 1000 / 60).toString() + ":"
-                binding.timerSeconds.text = (timeM / 1000 % 60).toString()
+                binding.timerMinutes.text = (timeM / 1000 / 60).toString() + ":" + (timeM / 1000 % 60).toString()
             }
 
             override fun onFinish() {
-                binding.timerSeconds.visibility = View.GONE
                 binding.timerMinutes.text = "Finish"
             }
 
