da8f9913b746f50c3eb324056f5f24ca3c80d831
Haggai Khafizov <hafyok5777@gmail.com>
2023-08-31 20:02:49 +0300

    Making menu with some examples

--- app/src/main/java/com/example/timerrest/TimerActivity.kt
+++ app/src/main/java/com/example/timerrest/TimerActivity.kt
@@ -3,12 +3,12 @@ package com.example.timerrest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.view.LayoutInflater
-import android.view.View
+import android.util.Log
 import android.widget.NumberPicker
 import android.widget.Toast
+import androidx.core.view.GravityCompat
 import com.example.timerrest.databinding.ActivityMainBinding
-import java.lang.String.format
+
 
 class TimerActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -17,15 +17,17 @@ class TimerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+
+
         val numberPicker: NumberPicker  = binding.numberPicker
         numberPicker.minValue = 0
-        numberPicker.maxValue = (180 - 15) / 5 // Количество возможных значений
+        numberPicker.maxValue = 33 // Количество возможных значений
 
         numberPicker.displayedValues = (15..180 step 5).map { it.toString() }.toTypedArray()
         numberPicker.wrapSelectorWheel = true
 
         numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
-            binding.textView.text = "Выбранное значение: ${numberPicker.displayedValues[newVal]}"
+            //binding.textView.text = "Выбранное значение: ${numberPicker.displayedValues[newVal]}"
             binding.timerMinutes.text = numberPicker.displayedValues[newVal] + ":00"
         }
 
@@ -33,6 +35,19 @@ class TimerActivity : AppCompatActivity() {
         binding.bStart.setOnClickListener {
             val selectedMinutes = numberPicker.displayedValues[numberPicker.value].toLong() // Выбранное количество минут
             startCountDownTimer(selectedMinutes * 60 * 1000)
+        }
+
+        binding.apply {
+            mainNavigationMenu.setNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.statistics -> Toast.makeText(this@TimerActivity, "Статистика", Toast.LENGTH_SHORT).show()
+                    R.id.shop -> Toast.makeText(this@TimerActivity, "Магазин", Toast.LENGTH_SHORT).show()
+                    R.id.achieve -> Toast.makeText(this@TimerActivity, "Достижения", Toast.LENGTH_SHORT).show()
+                    R.id.settings -> Toast.makeText(this@TimerActivity, "Настройки", Toast.LENGTH_SHORT).show()
+                }
+                mainDrawer.closeDrawer(GravityCompat.START)
+                true
+            }
         }
 
     }
